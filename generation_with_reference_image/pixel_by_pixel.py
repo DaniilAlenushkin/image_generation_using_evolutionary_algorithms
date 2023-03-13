46f1bc388bf5709068250a8c2e9bfdc288f067fd
Daniil Alenushkin <daniilalenushkin@yandex.ru>
2023-03-13 23:51:15 +0300

    fixed an algorithm for calculating each pixel separately and reorganize project

--- generation_with_reference_image/pixel_by_pixel.py
+++ generation_with_reference_image/pixel_by_pixel.py
@@ -1,13 +1,11 @@
 import numpy as np
-import multiprocessing
 from PIL import Image
 import time
 
-
-def get_initial_generation(population_size, chromosomes_number):
+def get_initial_generation():
     return [''.join(list(map(lambda x: str(x),
-                             list(np.random.randint(2, size=chromosomes_number)))))
-            for _ in range(population_size)]
+                             list(np.random.randint(2, size=CHROMOSOMES_NUMBER)))))
+            for _ in range(POPULATION_SIZE)]
 
 def fitness(bin_population, target_value):
     epsilon = 1e-10
@@ -17,16 +15,16 @@ def fitness(bin_population, target_value):
     norm_values = [i/sum(pre_norm_vector) for i in pre_norm_vector]
     return norm_values
 
-def reproduction(normalized_vector, bin_population, population_size):
+def reproduction(normalized_vector, bin_population):
     cum_weights = np.cumsum(normalized_vector)
-    spin = np.random.random(size=population_size) * cum_weights[-1]
+    spin = np.random.random(size=POPULATION_SIZE) * cum_weights[-1]
     indices = np.searchsorted(cum_weights, spin)
     return [bin_population[i] for i in indices]
 
-def crossing_over(bin_population, crossing_over_probability):
+def crossing_over(bin_population):
     np.random.shuffle(bin_population)
     for i in range(0, len(bin_population), 2):
-        if np.random.random() < crossing_over_probability:
+        if np.random.random() < CROSSING_OVER_PROBABILITY:
             number_of_genes = np.random.randint(len(bin_population[0]))
             direction = np.random.randint(2)
             if direction:  # faces
@@ -41,49 +39,54 @@ def crossing_over(bin_population, crossing_over_probability):
                 bin_population[i + 1] = bin_population[i + 1][:len(bin_population[i]) - number_of_genes] + back_1
     return bin_population
 
-def mutation(bin_population, mutation_probability):
+def mutation(bin_population):
     for i in range(len(bin_population)):
-        if np.random.random() < mutation_probability:
+        if np.random.random() < MUTATION_PROBABILITY:
             number_of_gen = np.random.randint(len(bin_population[i]))
             bin_population[i] = bin_population[i][:number_of_gen] + \
                                 str((int(bin_population[i][number_of_gen]) + 1) % 2) + \
                                 bin_population[i][number_of_gen + 1:]
     return bin_population
 
-def evolve_image(target_row):
-    POPULATION_SIZE = 1000
-    NUMBER_OF_GENERATIONS = 1000
-
-    MUTATION_PROBABILITY = 0.05
-    CROSSING_OVER_PROBABILITY = 0.5
-    CHROMOSOMES_NUMBER = 8
-    row_result = []
-    for target_pixel in target_row:
-        result_pixel = []
-        for channel in target_pixel:
-            population = get_initial_generation(POPULATION_SIZE, CHROMOSOMES_NUMBER)
-            for generation in range(NUMBER_OF_GENERATIONS):
-                norm_vector = fitness(population, channel)
-                population = reproduction(norm_vector, population, POPULATION_SIZE)
-                population = crossing_over(population, CROSSING_OVER_PROBABILITY)
-                population = mutation(population, MUTATION_PROBABILITY)
-            norm_vector = fitness(population, channel)
-            result_pixel.append(np.array(population[norm_vector.index(max(norm_vector))]))
-        row_result.append(np.array(result_pixel))
-    print('DONE')
-    return row_result
-
-
 if __name__ == '__main__':
     start_time = time.time()
     TARGET_IMAGE = 'images/target.png'
+    POPULATION_SIZE = 100
+    NUMBER_OF_GENERATIONS = 100
+    MUTATION_PROBABILITY = 0.05
+    CROSSING_OVER_PROBABILITY = 0.5
+    GENS_NUMBER = 4
+    CHROMOSOMES_NUMBER = 8
+
     target_image = Image.open(TARGET_IMAGE).convert('RGBA')
     target_array = np.array(target_image)
 
-    with multiprocessing.Pool(multiprocessing.cpu_count()-1) as pool:
-        result_image = pool.map(evolve_image, target_array)
-
+    height, width, channels = target_array.shape
+    result_image = []
+    # сначала по строкам
+    for h in range(1, height+1):
+        row_result = []
+        # Потом для каждого пикселя в строке
+        for w in range(1, width+1):
+            result_pixel = []
+            # Для каждой компоненты
+            for c in range(channels):
+                target = target_array[h-1:h, w-1:w,:][0][0][c]
+                population = get_initial_generation()
+                for generation in range(NUMBER_OF_GENERATIONS):
+                    norm_vector = fitness(population, target)
+                    population = reproduction(norm_vector, population)
+                    population = crossing_over(population)
+                    population = mutation(population)
+                norm_vector = fitness(population, target)
+                result_pixel.append(population[norm_vector.index(max(norm_vector))])
+            result_pixel = np.array(list(map(lambda x: int(x, 2), result_pixel)))
+            print(h, w, 'DONE')
+            row_result.append(np.array(result_pixel))
+        result_image.append(np.array(row_result))
     Image.fromarray(np.array(result_image).astype(np.uint8)).save(f"images/best_image.png")
     end_time = time.time()
     elapsed_time = end_time - start_time
     print("Программа выполнялась", elapsed_time, "секунд.")
+
+
